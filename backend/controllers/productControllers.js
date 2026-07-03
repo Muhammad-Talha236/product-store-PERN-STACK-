2da367af9c1f76bf9c7cd87b1062ec585219f094
Muhammad-Talha236 <mtalha.mt236@gmail.com>
2026-07-03 08:51:26 -0700

    add crud operations

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,5 @@
+export const getallProducts = (req, res) => {};
+export const getProduct = (req, res) => {};
+export const createProducts = (req, res) => {}; 
+export const updateProduct = (req, res) => {};
+export const deleteProduct = (req, res) => {};
